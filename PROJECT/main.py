56d97ca8754c1d7c88f87b55edd7e9607bc9cf61
xaimelix <xaimfolk355@yandex.ru>
2025-04-27 15:43:25 +1000

    end2

--- PROJECT/main.py
+++ PROJECT/main.py
@@ -30,7 +30,7 @@ swagger = Swagger(app, template={
         "description": "Документация для всех доступных API",
         "version": "1.0"
     },
-    "host": "prod-team-18-lkt02gu5.hack.prodcontest.ru"
+    "host": "prod-team-18-lkt02gu5.hack.prodcontest.ru",
     "basePath": "/",
     "schemes": [
         "http"
